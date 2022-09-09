0f9558ea0dba461eee42ea2bee42be859ebb8781
Matthew L. Bendall <mlb4001@med.cornell.edu>
2022-09-09 11:54:12 -0400

    load_stellarscope_report supports (cell x feature) or (feature x cell) matrices

--- R/load_stellarscope_report.R
+++ R/load_stellarscope_report.R
@@ -1,34 +1,58 @@
 #' Load count matrix from Stellarscope output files
 #'
-#' @param dirname Stellarscope output directory.
-#' @param count_file_suffix File name suffix for count matrix in MTX format. Default is 'TE_counts.mtx'.
-#' @param feature_file_suffix File name suffix for feature list. Default is 'features.tsv'.
-#' @param barcode_file_suffix File name suffix for barcode list. Default is 'barcodes.tsv'.
+#' @param stellarscope_dir Stellarscope output directory.
+#' @param exp_tag Experiment tag used in stellarscope run. Default is "stellarscope".
+#' @param count_file_suffix File name suffix for count matrix in MTX format.
+#'                          Default is 'TE_counts.mtx'.
+#' @param feature_file_suffix File name suffix for feature list. Default is
+#'                            'features.tsv'.
+#' @param barcode_file_suffix File name suffix for barcode list. Default is
+#'                            'barcodes.tsv'.
 #'
-#' @return A sparse matrix of of class "dgTMatrix" with features as rows and cells as columns.
+#' @return A sparse matrix of of class "dgTMatrix" with features as rows and
+#'         cells as columns.
 #' @export
 #'
 #' @examples
 load_stellarscope_report <-
-  function(dirname,
+  function(stellarscope_dir,
+           exp_tag = 'stellarscope',
            count_file_suffix = 'TE_counts.mtx',
            feature_file_suffix = 'features.tsv',
            barcode_file_suffix = 'barcodes.tsv') {
-    tecounts_mtx <- Sys.glob(file.path(dirname, paste0('*', count_file_suffix)))
-    features_tsv <- Sys.glob(file.path(dirname, paste0('*', feature_file_suffix)))
-    barcodes_tsv <- Sys.glob(file.path(dirname, paste0('*', barcode_file_suffix)))
 
-    ret <- Matrix::readMM(file = tecounts_mtx)
-    ret <- Matrix::t(ret)
+    prefix <- file.path(stellarscope_dir, sprintf('%s-', exp_tag))
+    tecounts_mtx <- Sys.glob(paste0(prefix, '*', count_file_suffix))
+    features_tsv <- Sys.glob(paste0(prefix, '*', feature_file_suffix))
+    barcodes_tsv <- Sys.glob(paste0(prefix, '*', barcode_file_suffix))
+
+    stopifnot(length(tecounts_mtx) == 1)
+    stopifnot(length(features_tsv) == 1)
+    stopifnot(length(barcodes_tsv) == 1)
+
+    counts <- Matrix::readMM(file = tecounts_mtx)
+    # counts <- Matrix::t(counts)
     features <- read.delim(file = features_tsv,
                            header = FALSE,
                            stringsAsFactors = FALSE)
     barcodes <- read.delim(file = barcodes_tsv,
                            header = FALSE,
                            stringsAsFactors = FALSE)
+    if(all(dim(counts) == c(nrow(barcodes), nrow(features)))) {
+        counts <- counts # print('input is barcode x feature (old way')
+    } else if (all(dim(counts) == c(nrow(features), nrow(barcodes)))) {
+        # print('input is feature x barcode (new way)')
+        counts <- Matrix::t(counts)
+    } else {
+      msg <- paste0(
+        sprintf("Matrix dimensions (%d, %d) ", nrow(counts), ncol(counts)),
+        sprintf("do not match barcode (%d) ", nrow(barcodes)),
+        sprintf("or feature (%d) dimensions.", nrow(features))
+      )
+      stop(msg)
+    }
+    rownames(counts) <- barcodes$V1
+    colnames(counts) <- features$V1
 
-    rownames(ret) <- features$V1
-    colnames(ret) <- barcodes$V1
-
-    ret
+    counts
   }
